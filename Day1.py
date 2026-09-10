4350464f8d62dca11655cf25cbf0ebdf4d55f7d1
Nadiya Sabha <syedanadiyasabha@gmail.com>
2026-09-10 16:25:32 -0700

    Day 1: Firstscript

---
+++ Day1.py
@@ -0,0 +1,8 @@
+Name=input("What is your name : ")
+print( f"Welcome  {Name}, to AI course" )
+def my_function(fname):
+    print("Hi " +fname+" Kumar")
+
+
+my_function("Rajesh")
+my_function("krishna")
